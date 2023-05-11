6129cf29842339fb10dac4aa29459b320cc65b1a
tdmadc107 <dangdat111@gmail.com>
2023-05-11 23:07:14 +0700

    Finally

--- app.js
+++ app.js
@@ -12,13 +12,21 @@ function Dino(species, weight, height, diet, where, when, fact) {
 // Create Dino Objects
 const dino = new Dino();
 
-// Create Human Object
+// Get data Dino Json
+const getDinoData = () =>
+  fetch('./dino.json')
+    .then(res => res.json())
+    .then(data => generateTile(data.Dinos));
+
+// Create Human Constructor
 function Human(name, height, weight, diet) {
   this.species = name;
   this.height = height;
   this.weight = weight;
   this.diet = diet;
 }
+
+// Create Human Object
 const human = new Human();
 
 // Use IIFE to get human data from form
@@ -28,86 +36,82 @@ const inInches = document.getElementById('inches');
 const inWeight = document.getElementById('weight');
 const inDiet = document.getElementById('diet');
 
+// Get Human data from DOM
 const getHumanData = (() => {
-  function getData() {
+  function setHumanData() {
     human.species = inName.value;
-    human.height = inFeet.value + inInches.value * 5;
+    // Convert feet to inches
+    human.height = inFeet.value * 12 + parseInt(inInches.value);
     human.weight = inWeight.value;
     human.diet = inDiet.value;
   }
   return {
-    human: getData,
+    human: setHumanData,
   }
 })();
 
-// Create Dino Compare Method 1
+// Create Dino Compare Weight
 // NOTE: Weight in JSON file is in lbs, height in inches. 
 Dino.prototype.compareWeight = () => {
   if (dino.weight > human.weight) {
-    dino.fact = `${dino.species} is ${dino.weight - human.weight} lbs heavier than ${human.species}`;
+    dino.fact = `${dino.species} living ${dino.when} at ${dino.where} is ${dino.weight - human.weight} lbs heavier than ${human.species}`;
     return dino.fact;
   } else {
-    dino.fact = `${dino.species} is ${human.weight - dino.weight} lbs lighter than ${human.species}`;
+    dino.fact = `${dino.species} living ${dino.when} at ${dino.where} is ${human.weight - dino.weight} lbs lighter than ${human.species}`;
     return dino.fact;
   }
 }
 
-// Create Dino Compare Method 2
+// Create Dino Compare Height
 Dino.prototype.compareHeight = () => {
-  if (dino.weight > human.weight) {
-    dino.fact = `${dino.species} is ${dino.weight - human.weight} inches taller than ${human.species}`;
+  if (dino.height > human.height) {
+    dino.fact = `${dino.species} living ${dino.when} at ${dino.where} is ${dino.height - human.height} inches taller than ${human.species}`;
     return dino.fact;
   } else {
-    dino.fact = `${dino.species} is ${human.weight - dino.weight} inches smaller than ${human.species}`;
+    dino.fact = `${dino.species} living ${dino.when} at ${dino.where} is ${human.height - dino.height} inches smaller than ${human.species}`;
     return dino.fact;
   }
 }
 
-// Create Dino Compare Method 3
+// Create Dino Compare Diet
 Dino.prototype.compareDiet = () => {
   if (human.diet === dino.diet) {
-    dino.fact = `${dino.species} is ${dino.diet} like ${human.species}`;
+    dino.fact = `${dino.species} living ${dino.when} at ${dino.where} is ${dino.diet} like ${human.species}`;
     return dino.fact;
   } else {
-    dino.fact = `${dino.species} is ${dino.diet} but ${human.species} is ${human.diet} `;
+    dino.fact = `${dino.species} living ${dino.when} at ${dino.where} is ${dino.diet} but ${human.species} is ${human.diet}. They are different`;
     return dino.fact;
   }
 }
 
-// Get data Dino Json
-const getDinoData = async () => {
-  const dinoJson = await fetch("./dino.json");
-  const data = await dinoJson.json();
-  generateTile(data.Dinos);
-};
-
 // Generate Tiles for each Dino in Array
-const generateTile = (dinoArr) => {
-  let dinoData = [];
-  const sifterArr = [1, 1, 1, 0, 0, 0, 0];
+const generateTile = (dinos) => {
+  let dinoDatas = [];
+  const indexDinos = [1, 0, 1, 0, 1, 0, 1];
+
   // Shuffle array
-  shuffle(sifterArr);
-  let pigeonIndex = dinoArr.findIndex((dino) => dino.species === 'Pigeon');
-  sifterArr.splice(pigeonIndex, 0, 0);
-  dinoArr.forEach((item, i) => {
+  shuffle(indexDinos);
+
+  let birdIndex = dinos.findIndex((dino) => dino.fact === 'All birds are living dinosaurs.');
+  indexDinos.splice(birdIndex, 0, 0);
+  dinos.forEach((item, i) => {
     dino.species = item.species
     dino.height = item.height
     dino.weight = item.weight
+    dino.when = item.when
+    dino.where = item.where
     dino.diet = item.diet
-    if (sifterArr[i]) {
-      let random = Math.floor(Math.random() * 3) + 1;
-      if (item instanceof Human) {
-        random = '';
-      }
+    if (indexDinos[i]) {
+      let random = Math.floor(Math.random() * 3);
       switch (random) {
+        case 0:
+          dino.compareWeight();
+          break;
         case 1:
-          dino.compareHeight(item.fact);
+          dino.compareHeight();
           break;
         case 2:
-          dino.compareWeight(item.fact);
-          break;
-        case 3:
-          dino.compareDiet(item.fact);
+          dino.compareDiet();
           break;
         default:
           break;
@@ -116,26 +120,29 @@ const generateTile = (dinoArr) => {
       dino.fact = item.fact;
     }
 
-    dinoData.push(JSON.parse(JSON.stringify(dino)));
+    // Insert data dino to dinoDatas
+    dinoDatas.push(JSON.parse(JSON.stringify(dino)));
   });
-  dinoData.splice(4, 0, human);
-  dinoData.forEach((item) => {
+  dinoDatas.splice(((dinoDatas.length / 2)), 0, human);
+  dinoDatas.forEach((item) => {
     addTilesToDOM(item);
   });
 }
 
 function shuffle(a) {
   for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(Math.random() * (i - 1));
     [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
 }
 
-// Add tiles to DOM
+// Get DOM element
 const grid = document.getElementById('grid');
 const form = document.getElementById('dino-compare');
 const buttonCompare = document.getElementById('btn');
+
+// Add tiles to DOM
 const addTilesToDOM = (dino) => {
   const div = document.createElement("div");
   div.className = "grid-item";
